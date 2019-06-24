4897ebee96504def3757e828a535b04692478511
Gupta <amrita.gupta@capgemini.com>
2019-06-24 14:09:24 +0530

    debug

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ const ServiceNow = new sn('dev49606', 'admin', '10Service@321');
 //     API for connection from servicenow ticket//
 ///////////////////////////////////////////
 app.post('/chatops', function (req, response) { 
-        if (err) return console.log(err);
-        //console.log(credentials)    
+     
 		 response.setHeader('Content-Type', 'application/json');
 		console.log("Display name ", req.body.queryResult.intent.displayName);
         switch (req.body.queryResult.intent.displayName) {				
